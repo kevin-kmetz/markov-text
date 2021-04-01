3859e78c6aaf16dc94ad3f0882135a14f2717d83
Kevin Kmetz <kev.kmetz@gmail.com>
2021-04-01 00:51:23 -0700

    Starting working on LetterNode. Implemented basic constructors, started fleshing out add methods. Need to implement comparable interface and methods relating to seeing if subnodes already exist.

--- MarkovText/LetterNode.java
+++ MarkovText/LetterNode.java
@@ -1,5 +1,63 @@
 package MarkovText;
 
-class LetterNode {
-	
+import java.util.TreeSet;
+
+class LetterNode implements Comparable {
+
+	private final char letter;
+	private boolean isTerminal;
+
+	TreeSet<LetterNode> subNodes = new TreeSet<LetterNode>();
+
+	LetterNode(String wordSegment) {
+
+		if (wordSegment.length() == 1) {
+
+			letter = wordSegment.charAt(0);
+			isTerminal = true;
+
+		} else if (wordSegment.length() > 1) {
+
+			letter = wordSegment.charAt(0);
+			isTerminal = false;
+
+			LetterNode lowerNode = new LetterNode(wordSegment.subString(1, wordsegment.length()-1));
+			subNodes.add(lowerNode);
+
+		} else {
+
+			// throw error;
+
+		}
+
+	}
+
+	boolean add(String wordSegment) {
+
+		if (wordSegment.length() == 1) {
+
+			isTerminal = true;
+
+		} else if (wordSegment.length() > 1) {
+
+			isTerminal = false;
+
+			char nextChar = wordSegment.charAt(1);
+
+			if (this.containsLetterNode(nextChar)) {
+
+				
+
+			} else {
+
+				LetterNode lowerNode = new LetterNode(wordSegment.subString(1, wordSegment.length()-1));
+				subNodes.add(lowerNode);
+
+			}
+
+
+		}
+
+	}
+
 }
--- MarkovText/WordTrie.java
+++ MarkovText/WordTrie.java
@@ -1,5 +1,5 @@
 package MarkovText;
 
 class WordTrie {
-	
+
 }
